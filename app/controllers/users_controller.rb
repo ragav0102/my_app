94205677c47bf67713446faa59cb01375eaa3421
ragav <ragavendren@freshdesk.com>
2016-07-27 15:38:55 +0530

    Sign up added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def create 
+  	@user = User.new(params[:user])
+  	if @user.save
+      flash[:success] = "Welcome to My App"
+      redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
 end
